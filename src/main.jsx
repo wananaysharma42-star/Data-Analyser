5e074ed241f240e23a6b4679c5cd9417b46aefeb
wananaysharma42-star <w.ananaysharma42@gmail.com>
2026-04-19 18:45:26 +0530

    chore: remove deprecated theme context, unused styles, and scratch test files

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,13 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { DataProvider } from "./context/DataContext";
-import { ThemeProvider } from "./context/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider>
-        <DataProvider>
-          <App />
-        </DataProvider>
-      </ThemeProvider>
+      <DataProvider>
+        <App />
+      </DataProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- test_preprocess.js
+++
@@ -1,3 +0,0 @@
-import { preprocessSingleData } from './src/utils/preprocess.js';
-const data = [{a: 1, b: "x"}, {a: 2, b: "y"}];
-console.log(JSON.stringify(preprocessSingleData(data), null, 2));
